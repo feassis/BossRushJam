6f8287b63fd702dd0a2ddb4da7e17c639c3583ac
feassis <felipe.assis.2002@gmail.com>
2024-01-06 13:04:41 -0300

    Mecha assembler implemented
    The idea is a script that assamble a mecha whe givem parts

---
+++ Assets/Scripts/Mecha/MechaBodyPart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MechaBodyPart : MonoBehaviour
+{
+    [SerializeField] private Transform leftArmSocket;
+    [SerializeField] private Transform rightArmSocket;
+    [SerializeField] private Transform legSocket;
+
+    public Transform GetLeftArmSocket() => leftArmSocket;
+
+    public Transform GetRightArmSocket() => rightArmSocket;
+
+    public Transform GetLegSocket() => legSocket;
+}
