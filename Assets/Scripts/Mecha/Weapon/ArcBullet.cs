d974b2f01b0e31c3015adf9775fb4dce0a7524dd
feassis <felipe.assis.2002@gmail.com>
2024-01-10 17:15:44 -0300

    Basic implementation of blue acid bomb

--- Assets/Scripts/Mecha/Weapon/ArcBullet.cs
+++ Assets/Scripts/Mecha/Weapon/ArcBullet.cs
@@ -4,11 +4,13 @@ public class ArcBullet: MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
 
-    private float dmg;
+    protected float dmg;
+    protected GameObject owner;
     
-    public void Setup(float damage, Vector3 initialVelocity)
+    public void Setup(float damage, Vector3 initialVelocity, GameObject owner)
     {
         dmg = damage;
         rb.velocity = initialVelocity;
+        this.owner = owner;
     }
 }
---
+++ Assets/Scripts/Mecha/Weapon/BlueAcidFlashBomb.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BlueAcidFlashBomb : ArcBullet
+{
+    [SerializeField] private BlueAcidPuddle acidPuddlePrefab;
+    [SerializeField] private float dmgTickPeriod;
+    [SerializeField] private float duration;
+    [SerializeField] private LayerMask obstacleMask;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor" || collision.gameObject.tag == "Obstacle")
+        {
+            BlueAcidPuddle puddle = Instantiate(acidPuddlePrefab);
+            Ray ray = new Ray(transform.position, Vector3.down);
+
+            Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, obstacleMask);
+
+
+            puddle.transform.position = raycastHit.point + new Vector3(0, 0.001f, 0);
+            puddle.Setup(dmg, dmgTickPeriod, duration, owner);
+
+            Destroy(gameObject);
+        }
+    }
+}
