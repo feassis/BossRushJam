30111df1e60db683cceb285585fdd2ff734ff7ab
feassis <felipe.assis.2002@gmail.com>
2024-01-17 16:02:00 -0300

    Implementing starting torso end arms

--- Assets/Scripts/Mecha/Mecha/MechaStats.cs
+++ Assets/Scripts/Mecha/Mecha/MechaStats.cs
@@ -10,14 +10,73 @@ public class MechaStats : MonoBehaviour
 
     private List<StatusEffect> effects = new List<StatusEffect>();
 
+    private List<StatusDuration> durations = new List<StatusDuration>();
+
+    private class StatusDuration
+    {
+        public StatusEffect Effect;
+        public float Duration;
+
+        public StatusDuration(StatusEffect effect, float duration)
+        {
+            Effect = effect;
+            Duration = duration;
+        }
+    }
+
+    private void Update()
+    {
+        List<StatusDuration> tempDurations = new List<StatusDuration>();
+        tempDurations.AddRange(durations);
+
+        foreach(var duration in tempDurations)
+        {
+            duration.Duration -= Time.deltaTime;
+
+            if (duration.Duration <= 0)
+            {
+                RemoveStatusEffect(duration.Effect);
+            }
+        }
+    }
+
     public void AddStatusEffect(StatusEffect statusEffect)
     {
+        if (effects.Contains(statusEffect))
+        {
+            return;
+        }
+
         effects.Add(statusEffect);
+    }
+
+    public void AddStatusEffectWithLifetime(StatusEffect statusEffect, float duration)
+    {
+        AddStatusEffect(statusEffect);
+
+        var effectDuration = durations.Find(d => d.Effect == statusEffect);
+
+        if (effectDuration != null)
+        {
+            effectDuration.Duration = duration;
+        }
+        else
+        {
+            durations.Add(new StatusDuration(statusEffect, duration));
+        }
+
     }
 
     public void RemoveStatusEffect(StatusEffect statusEffect)
     {
         effects.Remove(statusEffect);
+
+        var effectDuration = durations.Find(d => d.Effect == statusEffect);
+
+        if (effectDuration != null)
+        {
+            durations.Remove(effectDuration);
+        }
     }
 
     public bool HasStatus(StatusEffect statusEffect)
@@ -43,6 +102,7 @@ public class MechaStats : MonoBehaviour
         return stat switch
         {
             Stat.SPD => HasStatus(StatusEffect.Rooted) ? 0 : GetStat(Stat.SPD).Amount,
+            Stat.DEF => HasStatus(StatusEffect.DefenseDown30) ? GetStat(Stat.DEF).Amount * (0.7f) : GetStat(Stat.DEF).Amount, 
             _ => GetStat(stat).Amount
         };
     }
@@ -89,4 +149,7 @@ public enum StatusEffect
     None = 0,
     Rooted = 1,
     Invulnerable = 2,
+    DefenseDown30 =3,
+    DamageReduction50 = 4,
+
 }
