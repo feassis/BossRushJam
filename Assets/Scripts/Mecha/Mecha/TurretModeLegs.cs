8bf342e1f90ee6a2910e587c7241fca3baa38c22
feassis <felipe.assis.2002@gmail.com>
2024-01-21 13:30:50 -0300

    cephalo legs

---
+++ Assets/Scripts/Mecha/Mecha/TurretModeLegs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TurretModeLegs : MechaLegPart
+{
+    [SerializeField] private float duration;
+
+    private bool isOnTurretMode;
+
+
+    public override void OnLegAction()
+    {
+        if(isOnTurretMode)
+        {
+            return;
+        }
+
+        SpendManaAndAct(() =>
+        {
+            isOnTurretMode = true;
+
+            mechaStats.AddStatusEffectWithLifetime(StatusEffect.Rooted, duration);
+            mechaStats.AddStatusEffectWithLifetime(StatusEffect.Atk200, duration);
+            mechaStats.AddStatusEffectWithLifetime(StatusEffect.Int200, duration);
+            mechaStats.AddStatusEffectWithLifetime(StatusEffect.Def125, duration);
+            mechaStats.AddStatusEffectWithLifetime(StatusEffect.Wis125, duration);
+
+            StartCoroutine(CoolDownCycle());
+        });
+    }
+
+
+    private IEnumerator CoolDownCycle()
+    {
+        yield return new WaitForSeconds(duration);
+        isOnTurretMode = false;
+    }
+}
