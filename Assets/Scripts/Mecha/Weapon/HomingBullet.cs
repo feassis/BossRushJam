5d733577e8309d71932104e846bd5a81ed3a78e0
feassis <felipe.assis.2002@gmail.com>
2024-01-22 18:38:18 -0300

    implementing left hand of corruption

---
+++ Assets/Scripts/Mecha/Weapon/HomingBullet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class HomingBullet : Bullet
+{
+    [SerializeField] private float rotationSpeed = 30f;
+    private GameObject target;
+    public void SetHommingTarget(GameObject target)
+    {
+        this.target = target;
+    }
+
+    protected override void Update()
+    {
+        // Calculate the direction to the target
+        Vector3 directionToTarget = target.transform.position - transform.position;
+
+        // Ensure that the direction has a non-zero length
+        if (directionToTarget.sqrMagnitude > 0.001f)
+        {
+            // Flatten the direction to only consider the X-Z plane
+            directionToTarget.y = 0f;
+
+            // Create a rotation that looks towards the target in the X-Z plane
+            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget, Vector3.up);
+
+            // Smoothly rotate towards the target rotation
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        }
+
+        var desiredIntecrement = transform.forward * speed * Time.deltaTime;
+
+        desiredIntecrement.y = 0;
+
+        transform.position += desiredIntecrement;
+    }
+}
---
+++ Assets/Scripts/Mecha/Weapon/LeftHAndOfCorruptionBullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LeftHAndOfCorruptionBullet : HomingBullet
+{
+    [SerializeField] private float duration = 10f;
+    protected override void ApplyBulletDamage(IDamageable damageable, GameObject hitedObj)
+    {
+        if (hitedObj.TryGetComponent<MechaStats>(out MechaStats targetStats))
+        {
+            targetStats.AddStatusEffectWithLifetime(StatusEffect.Wis90, duration);
+        }
+
+        base.ApplyBulletDamage(damageable, hitedObj);
+    }
+}
