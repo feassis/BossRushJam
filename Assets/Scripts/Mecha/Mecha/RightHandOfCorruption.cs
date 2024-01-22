e75b23fe9eab6310f0d340bcfdbc4b3f1d5fef92
feassis <felipe.assis.2002@gmail.com>
2024-01-22 19:43:20 -0300

    implementing right hand of corruption

---
+++ Assets/Scripts/Mecha/Mecha/RightHandOfCorruption.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class RightHandOfCorruption : MechaArmPart
+{
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float gasLifetime = 12;
+    [SerializeField] private float coolDown = 15;
+    [SerializeField] private float tick = 0.2f;
+    [SerializeField] private float manaPercentageDamage = 0.02f;
+    [SerializeField] private RightHandOfCorruptionMist prefab;
+
+    private float timer = 0;
+
+    public override void OnAttackPressed(bool isplayerTarget = false)
+    {
+        base.OnAttackPressed(isplayerTarget);
+
+        if (timer > 0)
+        {
+            return;
+        }
+
+        SpendManaAndAct(() =>
+        {
+            var inst = Instantiate(prefab);
+            inst.transform.position = spawnPoint.position;
+
+            inst.Setup(mechaStats.gameObject);
+            inst.SetupDamage(GetDamage(), tick, gasLifetime, manaPercentageDamage);
+            timer = coolDown;
+        });
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        timer -= Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Mecha/Mecha/RightHandOfCorruptionMist.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RightHandOfCorruptionMist : SphereDetection
+{
+    private float manaPercentage;
+    private float damage;
+    private float tick;
+    private float duration;
+
+    private float tickTimer = 0;
+
+    public void SetupDamage(float dmgPerSecond, float tick, float duration, float manaPercentage)
+    {
+        damage = dmgPerSecond;
+        this.tick = tick;
+        this.duration = duration;
+        this.manaPercentage = manaPercentage;
+
+        StartCoroutine(Lifetime());
+    }
+
+    private void Update()
+    {
+        tickTimer -= Time.deltaTime;
+
+        if(tickTimer < 0)
+        {
+            tickTimer = tick;
+            foreach (var entitie in entitiesInRange)
+            {
+                entitie.TakeDamage(damage * tick);
+                if(entitie.gameObject.TryGetComponent<Mana>(out Mana mana))
+                {
+                    mana.SpendMana(mana.GetMaxMana() * manaPercentage);
+                }
+            }
+        }
+    }
+
+
+    private IEnumerator Lifetime()
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(gameObject);
+    }
+}
